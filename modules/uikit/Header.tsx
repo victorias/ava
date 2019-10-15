136b07b834b895390273f3b075cf1ea0d23cf7e1
victoria <victoria.s.sun@gmail.com>
2019-10-14 17:48:47 -0700

    header updates

--- modules/uikit/Header.tsx
+++ modules/uikit/Header.tsx
@@ -1,9 +1,41 @@
 import * as React from 'react';
 import { Fonts, FuturaWeights, MrsEavesWeights } from './constants';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  display: flex;
+  padding: 4rem;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+
+  ul {
+    display: flex;
+    font-family: ${Fonts.FUTURA_PT};
+    font-weight: ${FuturaWeights.BOOK};
+    font-size: 1.6rem;
+    flex-basis: 20rem;
+  }
+
+  ul:last-child {
+    justify-content: flex-end;
+  }
+
+  li + li {
+    margin-left: 2rem;
+  }
+
+  h1 {
+    text-align: center;
+    font-family: ${Fonts.MRS_EAVES};
+    font-weight: ${MrsEavesWeights.ROMAN};
+    font-size: 3.6rem;
+  }
+`;
 
 const Header = () => {
   return (
-    <header className="header">
+    <StyledHeader>
       <ul>
         <li>Shop</li>
         <li>Try</li>
@@ -14,34 +46,7 @@ const Header = () => {
       <ul>
         <li>Cart (0)</li>
       </ul>
-      <style jsx>{`
-        .header {
-          display: flex;
-          padding: 4rem;
-          justify-content: space-between;
-          align-items: center;
-          align-self: stretch;
-        }
-
-        ul {
-          display: flex;
-          font-family: ${Fonts.FUTURA_PT};
-          font-weight: ${FuturaWeights.BOOK};
-          font-size: 1.6rem;
-        }
-
-        li + li {
-          margin-left: 2rem;
-        }
-
-        h1 {
-          text-align: center;
-          font-family: ${Fonts.MRS_EAVES};
-          font-weight: ${MrsEavesWeights.ROMAN};
-          font-size: 3.6rem;
-        }
-      `}</style>
-    </header>
+    </StyledHeader>
   );
 };
 
