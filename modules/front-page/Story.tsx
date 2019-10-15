6d3fe3ac488c907cb3c23a218367708f83403eeb
victoria <victoria.s.sun@gmail.com>
2019-10-14 18:02:12 -0700

    story convert

--- modules/front-page/Story.tsx
+++ modules/front-page/Story.tsx
@@ -1,59 +1,58 @@
 import * as React from 'react';
 import Grid, { GUTTER } from '../uikit/Grid';
 import { Fonts, FuturaWeights, Colors } from '../uikit/constants';
+import styled from 'styled-components';
 
 interface Props {
   gridArea: string;
 }
 
+const StorySection = styled.section`
+  grid-column: span 12;
+  grid-area: ${(props: Props) => props.gridArea};
+  padding: ${3.5 * GUTTER}rem ${2 * GUTTER}rem;
+
+  h2 {
+    font-family: ${Fonts.FUTURA_PT};
+    font-weight: ${FuturaWeights.BOOK};
+    line-height: 1.2;
+    color: ${Colors.BLACK};
+    grid-column: span 6;
+    grid-area: title;
+    align-self: center;
+    font-size: 4.8rem;
+  }
+
+  p {
+    grid-area: body;
+    font-family: ${Fonts.FUTURA_PT};
+    font-weight: ${FuturaWeights.BOOK};
+    line-height: 1.2;
+    color: ${Colors.BLACK};
+    font-size: 1.6rem;
+  }
+`;
+
 const Story = ({ gridArea }: Props) => (
-  <>
-    <section>
-      <Grid
-        columns={2}
-        gridTemplateAreas={`
+  <StorySection gridArea={gridArea}>
+    <Grid
+      columns={2}
+      gridTemplateAreas={`
         "title body"
       `}
-      >
-        <h2>Say yes to the dress at home</h2>
-        <p>
-          Tempor ut sunt duis est laboris ut velit ex reprehenderit. Culpa
-          tempor ex do consequat qui ut et amet non nostrud fugiat. Eiusmod
-          fugiat qui consectetur proident consequat velit ipsum. Anim enim
-          nostrud sint irure occaecat cillum est consequat culpa ad elit Lorem
-          esse nisi. Ad excepteur sit voluptate laborum nisi anim ea. Cillum
-          tempor duis aute sit amet amet et est consectetur est aliqua mollit.
-        </p>
-      </Grid>
-    </section>
-    <style jsx>{`
-      section {
-        grid-column: span 12;
-        grid-area: ${gridArea};
-        padding: ${3.5 * GUTTER}rem ${2 * GUTTER}rem;
-      }
-
-      h2 {
-        font-family: ${Fonts.FUTURA_PT};
-        font-weight: ${FuturaWeights.BOOK};
-        line-height: 1.2;
-        color: ${Colors.BLACK};
-        grid-column: span 6;
-        grid-area: title;
-        align-self: center;
-        font-size: 4.8rem;
-      }
-
-      p {
-        grid-area: body;
-        font-family: ${Fonts.FUTURA_PT};
-        font-weight: ${FuturaWeights.BOOK};
-        line-height: 1.2;
-        color: ${Colors.BLACK};
-        font-size: 1.6rem;
-      }
-    `}</style>
-  </>
+      gap={5}
+    >
+      <h2>Say yes to the dress at home</h2>
+      <p>
+        Tempor ut sunt duis est laboris ut velit ex reprehenderit. Culpa tempor
+        ex do consequat qui ut et amet non nostrud fugiat. Eiusmod fugiat qui
+        consectetur proident consequat velit ipsum. Anim enim nostrud sint irure
+        occaecat cillum est consequat culpa ad elit Lorem esse nisi. Ad
+        excepteur sit voluptate laborum nisi anim ea. Cillum tempor duis aute
+        sit amet amet et est consectetur est aliqua mollit.
+      </p>
+    </Grid>
+  </StorySection>
 );
 
 export default Story;
--- modules/uikit/Grid.tsx
+++ modules/uikit/Grid.tsx
@@ -1,34 +1,28 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
 interface Props {
   children: React.ReactNode;
   className?: string;
   columns?: Number;
   gridTemplateAreas?: string;
+  gap?: Number;
 }
 
 export const GUTTER = 2; // em
 const WRAPPER = '100%';
 
-const Grid = ({
-  children,
-  columns = 12,
-  gridTemplateAreas = 'unset',
-}: Props) => (
-  <div className="grid">
-    {children}
-    <style jsx>{`
-      .grid {
-        display: grid;
-        grid-template-columns: repeat(${columns}, 1fr);
-        grid-auto-rows: auto;
-        grid-auto-flow: row;
-        grid-gap: 0em;
-        max-width: ${WRAPPER};
-        grid-template-areas: ${gridTemplateAreas};
-      }
-    `}</style>
-  </div>
-);
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(
+    ${(props: Props) => `${props.columns}` || '12'},
+    1fr
+  );
+  grid-auto-rows: auto;
+  grid-auto-flow: row;
+  grid-gap: ${(props: Props) => `${props.gap}` || `0`}em;
+  max-width: ${WRAPPER};
+  grid-template-areas: ${(props: Props) => props.gridTemplateAreas || 'unset'};
+`;
 
 export default Grid;
