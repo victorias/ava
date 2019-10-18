429fc91861547d954ce38332912c3aa747f051b3
victoria <victoria.s.sun@gmail.com>
2019-10-17 20:24:27 -0700

    gh setup

--- next.config.js
+++ next.config.js
@@ -2,24 +2,12 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
 const withImages = require('next-images');
 const withCSS = require('@zeit/next-css');
 
-module.exports = phase => {
-  if (phase === PHASE_DEVELOPMENT_SERVER) {
-    return withCSS(
-      withImages({
-        env: {
-          SHOPIFY_DOMAIN: 'ava-bridal-dev.myshopify.com',
-          SHOPIFY_STOREFRONT_ACCESS_TOKEN: 'be12b01f2eaf817739d04cac04b248ff',
-        },
-      })
-    );
-  }
-
-  return withCSS(
-    withImages({
-      env: {
-        SHOPIFY_DOMAIN: 'ava-bridal',
-        SHOPIFY_STOREFRONT_ACCESS_TOKEN: '596f04f97e83f24b8b244b80d6857177',
-      },
-    })
-  );
-};
+module.exports = withCSS(
+  withImages({
+    exportPathMap: function() {
+      return {
+        '/': { page: '/' },
+      };
+    },
+  })
+);
